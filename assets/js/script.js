15f5aa7dcc1da08f8a73befb4a3fbff8957fb4a7
AZLee <amjedzehani@gmai.com>
2024-10-18 13:54:19 +0200

    reparation du menu mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 const btn = document.querySelector(".menu");
-const nav = document.querySelector(".nav-links")
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav-links a");
 
 btn.addEventListener('click',()=>{
     nav.classList.toggle('mobile-nav');
-    btn.classList.toggle('open');
+    btn.classList.toggle('open'); 
 })
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.remove('mobile-nav'); 
+        btn.classList.remove('open');
+    });
+});
