88df4e205dfd9c464cba25592ff23ed1ad7f7ce8
AZLee <amjedzehani@gmai.com>
2025-03-08 09:27:05 +0100

    vite maj

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build',  // Définir le répertoire de sortie comme 'build'
+  },
 })
