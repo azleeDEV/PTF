a79658a1cecc24e23ca8845296270d45e3709ddb
AZLee <amjedzehani@gmai.com>
2025-03-09 08:41:52 +0100

    maj

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,11 +6,11 @@ const Header = () => {
     <div id="a-propos" className="header-wrapper">
       <div className="a-propos-container">
           <p><span className="span-p">Salut, moi c’est AZ !</span><br/>
-          Développeur informatique passionné,<br/> 
-          je conçois des interfaces web dynamiques et optimisées, 
-          avec une attention particulière à l’expérience utilisateur, 
-          je développe aussi bien pour le web que pour la blockchain, 
-          en alliant performance, esthétique et code soigné.</p>
+          Développeur passionné, 
+          je crée des interfaces web et blockchain dynamiques, 
+          performantes et optimisées. Mon objectif ? 
+          Offrir une expérience utilisateur fluide et intuitive en alliant design, 
+          performance et code de qualité.</p>
           <img src={Logo} alt="header" />
       </div>
       <div className="tags-wrapper">
